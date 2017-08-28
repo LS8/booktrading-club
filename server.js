ed5774e4e07e1f687206fe5ba8e111444dcfccf5
LS8 <lennartschoettker@hotmail.com>
2017-08-28 20:09:03 +0200

    add connection to postgresql // use routes

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 const path = require('path');
-// const routes = require('./routes'); Routes
+const routes = require('./routes');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: config.dbAddress,
+});
+client.connect();
+
 
 // Common Middleware uncomment and npm install when required
 
 // const cors = require('cors');
 // const bodyparser = require('body-parser');
-
-
-// const mongoose = require('mongoose');
-// mongoose.connect(config.mongoAddress, { useMongoClient: true });
-// mongoose.connection.on('connected', () => { console.log(`Connected to database on ${config.mongoAddress}`); });
-// mongoose.connection.on('error', (err) => { console.log(`Database error: ${err} while trying to connect on ${config.mongoAddress}`);});
 
 // const passport = require('passport');
 // app.use(passport.initialize());
@@ -29,7 +30,7 @@ app.get('/', function (req, res) { // let frontend handle client side routing
 });
 
 // Use Routes
-// app.use('/api', routes);
+app.use('/api', routes);
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
