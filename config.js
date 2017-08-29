61a569e353fd6154ef06a3170cdee371990db714
LS8 <lennartschoettker@hotmail.com>
2017-08-29 19:46:45 +0200

    have localdb name before herokus uri for local development

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const secrets = require('./secrets');
 module.exports = {
   port: process.env.PORT || 8080,
-  dbAddress: process.env.DATABASE_URL || secrets.db || 'booktrading',
+  dbAddress: process.env.DATABASE_URL || 'booktrading' || secrets.db,
 }
 
