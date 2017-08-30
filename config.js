b0acf19aad863d07bdd1ecdfa621c0e0cd864237
LS8 <lennartschoettker@hotmail.com>
2017-08-30 16:23:18 +0200

    add cookie secret

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ const secrets = require('./secrets');
 module.exports = {
   port: process.env.PORT || 8080,
   dbAddress: process.env.DATABASE_URL || 'booktrading' || secrets.db,
+  cookieSecret: secrets.cookie
 }
 
