a603e6c7ac86d80da6bc0fc23e204c1c1759bf9b
LS8 <lennartschoettker@hotmail.com>
2017-09-01 23:49:07 +0200

    add login post route and function that checks the session to see if the user is logged in

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,28 @@ const Router = express.Router();
 
 const User = require('../models/user');
 
+const loggedIn = (req) => {
+  if (req.session.user && req.session.cookie) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Router.post('/login', (req ,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ where: { username: username } }).then(function (user) {
+    if (!user) {
+      res.json({ success: false, msg: 'User not found', status: 1 });
+    } else if (!user.validPassword(password, user)) {
+      res.json({ success: false, msg: 'Wrong password', status: 2 });
+    } else {
+      req.session.user = user.dataValues;
+      res.json({ success: true, msg: 'Login was successfull', status: 0, user: {username: user.username, email: user.email} });
+    }
+  });
+});
 
 Router.post('/signup', (req, res) => {
   User.create({
