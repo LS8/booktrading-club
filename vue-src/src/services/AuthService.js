3abd786c96dec3df05194ec05187d19eb7f98de6
LS8 <lennartschoettker@hotmail.com>
2017-09-02 00:00:11 +0200

    add auth service

---
+++ vue-src/src/services/AuthService.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import config from '../config';
+
+const url = config.serverAddress;
+
+export default {
+  login(formData) {
+    return Vue.http.post(`${url}/api/login`, {
+      username: formData.username,
+      password: formData.password
+    })
+    .then(onSuccess, onError);
+  },
+  register(formData) {
+    return Vue.http.post(`${url}/api/register`, {
+      username: formData.username,
+      email: formData.email,
+      password: formData.password
+    })
+    .then(onSuccess, onError);
+  },
+  logOut() {
+    localStorage.clear();
+  },
+  loggedIn() {
+    return localStorage.getItem('user') ? true : false;
+  }
+}
+
+const onSuccess = (response) => {
+  return response.json();
+}
+
+const onError = (error) => {
+  return console.log(error);
+}
