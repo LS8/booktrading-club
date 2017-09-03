4b83eb391ae25b5d6b75150ca9d821c5f49baf87
LS8 <lennartschoettker@hotmail.com>
2017-09-03 11:26:43 +0200

    use vuex // add a store that tracks authentication in its state

--- vue-src/src/main.js
+++ vue-src/src/main.js
@@ -2,18 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import ('../node_modules/vuetify/dist/vuetify.min.css')
 import Vue from 'vue'
+import Vuex from 'vuex'
 import VueResource from 'vue-resource'
 import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
+import AuthService from './services/AuthService'
 
+Vue.use(Vuex)
 Vue.use(VueResource)
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+    authenticated: AuthService.loggedIn()
+  },
+  mutations: {
+    login: state => state.authenticated = true,
+    logout: state => state.authenticated = false
+  },
+   getters: {
+     auth: state => {
+       return state.authenticated;
+     }
+   }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }
