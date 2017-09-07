1af35c36129b5167464d0c891e341c31dc850daf
LS8 <lennartschoettker@hotmail.com>
2017-09-07 18:52:42 +0200

    add vuetify layout and form to search for a book

--- vue-src/src/components/Profile.vue
+++ vue-src/src/components/Profile.vue
@@ -1,6 +1,30 @@
 <template>
 <div>
   <h1>Profile</h1>
+  <v-layout row wrap>
+
+    <v-flex xs12 sm6>
+      <h2>Your books</h2>
+    </v-flex>
+
+    <v-flex xs12 sm6>
+      <h2>Add a book</h2>
+        <form @submit.prevent="onSubmit">
+          <v-layout row>
+            <v-flex xs12 sm8 offset-sm2>
+              <v-text-field
+              v-model="searchTerm"
+              name="searchTerm"
+              label="Search the library"
+              required
+              ></v-text-field>
+              <v-btn type="submit" primary :disabled="!formIsValid">Submit</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+    </v-flex>
+
+  </v-layout>
 </div>
 </template>
 <script>
