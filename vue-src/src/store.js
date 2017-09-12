56b8359f1536f9a46a744c9e2151b4b5640d5eb2
LS8 <lennartschoettker@hotmail.com>
2017-09-12 11:55:40 +0200

    set intial userId and add action and mutation to set the user id

--- vue-src/src/store.js
+++ vue-src/src/store.js
@@ -2,25 +2,41 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import AuthService from './services/AuthService'
 
+const user = JSON.parse(localStorage.getItem('user')) || null;
+
+const userId = user ? user.id : null;
+
 Vue.use(Vuex);
 
 const state = {
-  authenticated: AuthService.loggedIn()
+  authenticated: AuthService.loggedIn(),
+  userId: userId
 };
 
 const mutations =  {
   login: state => state.authenticated = true,
-  logout: state => state.authenticated = false
+  logout: state => state.authenticated = false,
+  setUserId: (state, value) => state.userId = value
 };
 
 const getters = {
   auth: state => {
     return state.authenticated;
+  },
+  user: state => {
+    return state.userId;
   }
 };
+
+const actions = {
+  setUserId({ commit }, value) {
+    commit('setUserId', value)
+  }
+}
 
 export default new Vuex.Store({
   state,
   getters,
-  mutations
+  mutations,
+  actions
 })
