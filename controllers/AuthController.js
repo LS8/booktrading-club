c5db9b3c5d072469fc9b9f0a48f793018e31a7f9
LS8 <lennartschoettker@hotmail.com>
2017-09-17 14:42:25 +0200

    add async login method that also uses jwt to sign a user token

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,13 @@
 const { User } = require('../models');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+function jwtSignUser (user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7;
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  });
+}
 
 module.exports = {
   async register (req, res) {
@@ -16,6 +25,32 @@ module.exports = {
       } else {
         res.json({ success: false, msg: 'Error', status: 3, err: err });
       }
+    }
+  },
+  async login (req, res) {
+    try {
+      const { username , password } = req.body;
+      const user = await User.findOne({
+        where: {
+          username: username
+        }
+      });
+      console.log(user);
+      if (!user) { return res.json({ success: false, msg: 'User not found', status: 1 }); }
+      const isPasswordValid = await user.comparePassword(password);
+      if (!isPasswordValid) {
+        return res.json({ success: false, msg: 'Wrong password', status: 2 });
+      }
+      req.session.user = user.dataValues;
+      res.json({
+        success: true, 
+        msg: 'Login was successfull',
+        status: 0,
+        user: {username: user.username, email: user.email, id: user.id },
+        token: jwtSignUser(user.toJSON())
+      });
+    } catch (err) {
+      res.json({ success: false, msg: 'Error while trying to log in', status: 1, err: err });
     }
   }
 }
