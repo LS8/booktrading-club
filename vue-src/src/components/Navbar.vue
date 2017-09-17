b41e30145a3f23e75f61ab62d85276e95bdfdb75
LS8 <lennartschoettker@hotmail.com>
2017-09-17 17:45:45 +0200

    display and hide toolbar links when user is (not) logged in

--- vue-src/src/components/Navbar.vue
+++ vue-src/src/components/Navbar.vue
@@ -9,8 +9,10 @@
     >
       <v-list>
         <v-list-tile to='/home'><v-list-tile-content>Home</v-list-tile-content></v-list-tile>
-        <v-list-tile to='/login'><v-list-tile-content>Login</v-list-tile-content></v-list-tile>
-        <v-list-tile to='/register'><v-list-tile-content>Register</v-list-tile-content></v-list-tile>
+        <v-list-tile v-if="!$store.state.isUserLoggedIn" to='/login'><v-list-tile-content>Login</v-list-tile-content></v-list-tile>
+        <v-list-tile v-if="!$store.state.isUserLoggedIn" to='/register'><v-list-tile-content>Register</v-list-tile-content></v-list-tile>
+        <v-list-tile v-if="$store.state.isUserLoggedIn" to='/profile'><v-list-tile-content>Profile</v-list-tile-content></v-list-tile>
+        <v-list-tile v-if="$store.state.isUserLoggedIn" to='#' @click.native="onLogout"><v-list-tile-content>Logout</v-list-tile-content></v-list-tile>
       </v-list>
     </v-navigation-drawer>
 
@@ -21,9 +23,9 @@
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn flat to='/home'>Home</v-btn>
       <v-btn v-if="!$store.state.isUserLoggedIn" flat to='/login'>Login</v-btn>
+      <v-btn v-if="!$store.state.isUserLoggedIn" flat to='/register'>Register</v-btn>
       <v-btn v-if="$store.state.isUserLoggedIn" flat to='/profile'>Profile</v-btn>
       <v-btn v-if="$store.state.isUserLoggedIn" @click.native="onLogout" flat>Logout</v-btn>
-      <v-btn v-if="!$store.state.isUserLoggedIn" flat to='/register'>Register</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </div>
@@ -43,7 +45,8 @@ export default {
   methods: {
     onLogout () {
       AuthService.logout();
-      this.$store.commit('logout');
+      this.$store.dispatch('setToken', null);
+      this.$store.dispatch('setUser', null);
       this.$swal({
         title: 'Info',
         text: 'You are now logged out',
