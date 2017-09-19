b98b5b092a6bae72cf96044176c86478d0498c78
LS8 <lennartschoettker@hotmail.com>
2017-09-19 21:44:08 +0200

    add requesteddBy field

--- models/Book.js
+++ models/Book.js
@@ -16,6 +16,10 @@ module.exports = (sequelize, DataTypes) =>
     },
     imageLink: {
       type: DataTypes.STRING
+    },
+    requestedBy: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
     }
   },
     {
