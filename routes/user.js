abc393ee59973ba70c45bf8f3f946dbe0ce1a70e
LS8 <lennartschoettker@hotmail.com>
2017-09-25 11:41:25 +0200

    remove session and cookies

--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,6 @@ const { User } = require('../models');
 const AuthController = require('../controllers/AuthController');
 const isAuthenticated = require('../isAuthenticated');
 
-const loggedIn = (req) => {
-  if (req.session.user && req.session.cookie) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
 Router.post('/login', AuthController.login);
 
 Router.post('/register', AuthController.register);
@@ -19,25 +11,5 @@ Router.post('/register', AuthController.register);
 Router.get('/settings/:id', isAuthenticated, AuthController.getSettings);
 
 Router.post('/settings/:id', isAuthenticated, AuthController.postSettings);
-
-Router.get('/logout', (req, res) => {
-  res.clearCookie('user');
-  res.redirect('/api/profile');
-})
-
-Router.get('/session', (req, res) => {
-  req.session.destroy();
-  res.redirect('/api/profile');
-})
-
-
-Router.get('/profile', (req, res) => {
-  console.log(req.session);
-  if (loggedIn(req)) {
-    res.json({ success: true, user: req.session.user });
-  } else {
-    res.json({ success: false, msg: 'User is not logged in'});
-  }
-});
 
 module.exports = Router;
--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const history = require('connect-history-api-fallback');
 const path = require('path');
 const bodyparser = require('body-parser');
-const session = require('express-session');
 const app = express();
 const config = require('./config');
 const userRoutes = require('./routes/user');
 const bookRoutes = require('./routes/book');
 const { sequelize } = require('./models');
-
-// Common Middleware uncomment and npm install when required
 
 const cors = require('cors');
 app.use(cors());
@@ -17,25 +14,6 @@ app.use(cors());
 app.use(bodyparser.json());
 
 if (process.env.PROD_HEROKU) { app.use(history()); }
-
-// initialize express-session
-app.use(session({
-  name: 'user',
-  secret: config.cookieSecret,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 6000000
-  }
-}));
-
-// if cookie still present when user is not set then delete cookie (log out)
-app.use((req, res, next) => {
-  if (req.session.cookie && !req.session.user) {
-    res.clearCookie('user');
-  }
-  next();
-});
 
 require('./passport');
 
@@ -53,7 +31,4 @@ sequelize.sync()
   .then(() => {
     app.listen(config.port);
     console.log(`Server running on port ${config.port}`);
-  })
-
-// app.listen(config.port, () => {
-// });
+  });
