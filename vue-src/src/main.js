e42a0f3098d608c9861770266d78ed03f0fb0260
LS8 <lennartschoettker@hotmail.com>
2017-09-25 10:36:23 +0200

    set bearer token as authorization header

--- vue-src/src/main.js
+++ vue-src/src/main.js
@@ -19,6 +19,7 @@ Vue.use(Vuetify)
 Vue.use(VueSwal)
 Vue.config.productionTip = false
 
+Vue.http.headers.common['Authorization'] = `Bearer ${store.state.token}`;
 Vue.component('panel', Panel)
 
 sync(store, router)
